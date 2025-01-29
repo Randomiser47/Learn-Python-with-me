1fed514a2208b9fe810b35baedde2bc922cc8727
Hassaan Alvi <110996624+Randomiser47@users.noreply.github.com>
2025-01-30 02:00:14 +0500

    Add files via upload

---
+++ day1/day1.py
@@ -0,0 +1,47 @@
+# Printing
+# print('Day 1')
+# print('The fucntion is declared like this:')
+# print("print('what to print')")
+
+# String manipulation:
+# print("hellow world\n Hellow world")
+# print("hello" +" "+"Angela")
+
+# #Debuggin:
+# print("Day1 - String Manipulation")
+# print("String Concatenation is done with the '+' Sign.")
+# print('e.g. print("Hello" + "world")')
+# print("New lines can be created with a backlash and n.")
+
+#input
+# #input("STATE YOUR NAME:")
+# print('Hello' + ' ' +input('What is your name?')) #concatenation
+
+#Counting number of letters in a name
+# print(len(input("what is your name: ")))
+
+#Variables in Python
+# name = input('What is your name: ')
+# length = len(name)
+# print(length)
+
+#Variable Exercise
+# a = input('a: ')
+# b = input('b: ')
+# c=a
+# a=b
+# b=c
+# print("a: "+a)
+# print("b: "+b)
+
+#Naming Variables
+#Make Code Readable so that it makes sense to you when you come back after 2 years you don't look at it and go hein? 
+#can't use privilleged words for variable names (very very bad practice)
+
+#Day1 Project
+#Band name Generator
+print("Welcome to the Band Name Generatorrrrrrr!!!!")
+city = input("Please enter the city you are from: \n")
+pet = input("Please enter the name of your pet: \n")
+print("Your band name could be" + " " + city + " " + pet)
+
